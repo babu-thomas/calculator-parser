dc7c63dd24f81cffec13a9c20ff36e82ae36732e
Babu Thomas <babuthomas138@gmail.com>
2017-10-14 08:37:50 +0530

    Implement tokenizer

--- main.py
+++ main.py
@@ -20,10 +20,55 @@ class InputStream:
         print (msg, ' (column: ', self.col, ')', sep="")
 
 
+# '2+3' -> {type:num, value: 2}, {type:op, value:'+'}, {type:num, value: 3}
+class TokenStream:
+    def __init__(self, input_stream):
+        self.input_stream = input_stream
+
+    def is_whitespace(self, char):
+        return char in ' \t'
+
+    def is_digit(self, char):
+        return char.isdigit()
+
+    def is_operator(self, char):
+        return char in '+'
+
+    def read_while(self, predicate_func):
+        _str = ""
+        while not self.input_stream.is_eof() and predicate_func(self.input_stream.peek()):
+            _str += self.input_stream.next()
+        return _str
+
+    def read_number(self):
+        number = self.read_while(self.is_digit)
+        return {'type': 'num', 'value': int(number)}
+
+    def read_operator(self):
+        operator = self.read_while(self.is_operator)
+        return {'type': 'op', 'value': operator}
+
+    def read_next(self):
+        _ = self.read_while(self.is_whitespace)
+        if self.input_stream.is_eof():
+            return None
+        char = self.input_stream.peek()
+        if self.is_digit(char):
+            return self.read_number()
+        if self.is_operator(char):
+            return self.read_operator()
+        self.input_stream.croak("Can't handle character: " + char)
+        self.input_stream.next()
+        return None
+
+
 def main():
-    scanner = InputStream('Heya')
-    scanner.next()
-    scanner.croak('Something happened')
+    scanner = InputStream('  2 + 3  \n')
+    tokenizer = TokenStream(scanner)
+    token = tokenizer.read_next()
+    while token:
+        print(token)
+        token = tokenizer.read_next()
 
 
 if __name__ == '__main__':
