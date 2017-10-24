ac46742f0cbc2d584f69a61a4aada5f0a9a76d01
Babu Thomas <babuthomas138@gmail.com>
2017-10-24 14:52:57 +0530

    Add basic REPL

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from parser import Parser
 
-
 def main():
-    parser = Parser(' 12 + 3 * 4 ')
-    print(parser.parse())
-
+    print('Calculator v0.0.1\nWelcome! Type any expression below then press ENTER to evaluate.\n'
+        'Press ENTER on empty input to exit.')
+    _input = '';
+    while True:
+        _input = input('>> ')
+        if _input == '':
+            break
+        parser = Parser(_input)
+        print(parser.parse())
 
 if __name__ == '__main__':
     main()
