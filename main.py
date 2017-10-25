5239bb257272984a5576f924d311ad75882cd727
Babu Thomas <babuthomas138@gmail.com>
2017-10-25 22:14:40 +0530

    No semicolons in this town

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from parser import Parser
 def main():
     print('Calculator v0.0.1\nWelcome! Type any expression below then press ENTER to evaluate.\n'
         'Press ENTER on empty input to exit.')
-    _input = '';
+    _input = ''
     while True:
         _input = input('>> ')
         if _input == '':
